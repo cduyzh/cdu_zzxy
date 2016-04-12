7bba3f017da406c67d839c16a9f1adb3d9ba6425
zhangzewei <zhangzewei@testbird.com>
2016-04-12 16:23:41 +0800

    后台管理界面，不用切页面了，到时候我们一起弄的时候，直接给你讲，因为这个页面结构很清晰，而且大致相似

---
+++ manager/media/js/login.js
@@ -0,0 +1,67 @@
+var Login = function () {
+
+    return {
+        //main function to initiate the module
+        init: function () {
+
+           $('.login-form').validate({
+	            errorElement: 'label', //default input error message container
+	            errorClass: 'help-inline', // default input error message class
+	            focusInvalid: false, // do not focus the last invalid input
+	            rules: {
+	                username: {
+	                    required: true
+	                },
+	                password: {
+	                    required: true
+	                },
+	                remember: {
+	                    required: false
+	                }
+	            },
+
+	            messages: {
+	                username: {
+	                    required: "用户名是必填的！"
+	                },
+	                password: {
+	                    required: "密码是必填的！"
+	                }
+	            },
+
+	            invalidHandler: function (event, validator) { //display error alert on form submit   
+	                $('.alert-error', $('.login-form')).show();
+	            },
+
+	            highlight: function (element) { // hightlight error inputs
+	                $(element).closest('.control-group').addClass('error'); // set error class to the control group
+	            },
+
+	            success: function (label) {
+	                label.closest('.control-group').removeClass('error');
+	                label.remove();
+	            },
+
+	            errorPlacement: function (error, element) {
+	                error.addClass('help-small no-left-padding').insertAfter(element.closest('.input-icon'));
+	            },
+
+	            submitHandler: function (form) {
+	                window.location.href = "index.html";
+	            }
+	        });
+
+	        $('.login-form input').keypress(function (e) {
+	            if (e.which == 13) {
+	                if ($('.login-form').validate().form()) {
+	                    alert('登录成功');
+	                }
+	                return false;
+	            }
+	        });
+
+        }
+
+    };
+
+}();
