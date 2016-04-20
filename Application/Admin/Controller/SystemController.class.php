7965c56f379046ee2012a06f3054fc20404c7011
Mitol <986078867@qq.com>
2016-04-21 01:48:05 +0800

    后台首页模块显示控制,系统参数设置,角色创建修改

--- Application/Admin/Controller/SystemController.class.php
+++ Application/Admin/Controller/SystemController.class.php
@@ -13,6 +13,9 @@ use Think\Controller;
 
 class SystemController  extends SuperController
 {
+    /**
+     * show
+     */
     public function index() {
         $pageName = '系统参数设置';
         $setting['sitename'] = M('settings')->where('variable = \'sitename\'')->select()[0];
@@ -29,6 +32,9 @@ class SystemController  extends SuperController
         $this->display('System/main');
     }
 
+    /**
+     * update
+     */
     public function update() {
         $num = 0;
         foreach(I() as $key=>$item) {
@@ -47,9 +53,59 @@ class SystemController  extends SuperController
         $this->success('修改成功!');
     }
 
+    /**
+     * show
+     */
     public function home() {
         $pageName = '首页调用设置';
-        $this->assign(compact(['pageName']));
+        $modules = M('settings')->where('variable like \'siteindex%\'')
+            ->order('variable asc')->select();
+        foreach($modules as $key=>$module) {
+            $modules[$key]['value'] = explode(',', $module['value']);
+        }
+        $options = M('sitemodule')->where('fid != 0')->select();
+        $this->assign(compact(['pageName', 'modules', 'options']));
         $this->display('System/home');
+    }
+
+    /**
+     * set home module show
+     */
+    public function homeSet() {
+        if($_POST != null) {
+            for($i=0; $i<7; $i++) {
+                $flag = 0;
+                $flag = M('settings')->where("variable = '". I('index'.$i) ."'") ->count();
+                $flag += M('sitemodule')->where('id ='. I('siteindex'.$i))->count();
+                if($flag < 2) {
+                    $this->error("不能有数据为空!第".($i+1)."行数据填写错误!");
+                }
+                if(!is_numeric(I('num'.$i)) || I('num'.$i)<1) {
+                    $this->error("第".($i+1)."行调用条数填写错误!必须是大于0的整数");
+                }
+
+                if(!is_numeric(I('words'.$i)) || I('words'.$i)<1) {
+                    $this->error("第".($i+1)."行标题字数填写错误!必须是大于0的整数");
+                }
+                try {
+                    date(I('date'.$i));
+                } catch(\Exception $e) {
+                    $this->error("第".($i+1)."行时间格式填写错误!请修改!");
+                }
+                $datas[$i]['variable'] = I('index'.$i);
+                $datas[$i]['value'] = I('siteindex'.$i) . ',' .
+                                    I('num'.$i) . ',' .
+                                    I('words'.$i) . ',' .
+                                    I('date'.$i);
+            }
+        }
+        foreach($datas as $data) {
+            try {
+                M('settings')->where('variable=\''.$data['variable'].'\'')->data($data)->save();
+            } catch(\PDOException $e) {
+                $this->error("服务器错误!请刷新重试!");
+            }
+        }
+        $this->success(" 更新数据成功!");
     }
 }
